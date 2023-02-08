4e255d580a3d9cf6d4b2a4ccc5a5ce69b0330f4f
fairyninjaCM <fairyninjacm@gmail.com>
2023-02-07 19:30:32 -0600

    added the victorspx motor controllers

--- 4256-23-Volta/src/main/java/frc/robot/subsystems/Elevator.java
+++ 4256-23-Volta/src/main/java/frc/robot/subsystems/Elevator.java
@@ -10,14 +10,20 @@ import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
 
 public class Elevator {
 
     private static DoubleSolenoid tiltSolenoid;
+    private final VictorSPX baseMotor1;
+    private final VictorSPX baseMotor2;
     
     
     public Elevator(){
-        
+        baseMotor1 = new VictorSPX(Constants.MOTOR_ID_PLACEHOLDER);
+        baseMotor2 = new VictorSPX(Constants.MOTOR_ID_PLACEHOLDER);
         tiltSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, Constants.PNEUMATIC_ID_PLACEHOLDER, Constants.PNEUMATIC_ID_PLACEHOLDER);
     }
     
@@ -30,11 +36,15 @@ public class Elevator {
     }
 
     public void extendElevatorTop() {
-        
+        baseMotor1.set(ControlMode.PercentOutput, Constants.EXTENSION_MOTOR_SPEED);
     }
 
     public void retractElevatorBottom() {
-        
+        baseMotor1.set(ControlMode.PercentOutput, Constants.RETRACTION_MOTOR_SPEED);
+    }
+
+    public void setElevator(int encoderCounts) {
+
     }
 
 
